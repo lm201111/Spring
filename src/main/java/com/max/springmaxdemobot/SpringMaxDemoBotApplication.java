eb30b08dbe752bb81e14bb2aa617606f708ce902
Max Lizunov <lm201111@mail.ru>
2022-11-28 15:25:52 +0400

    Демо версия телеграм-бота на spring.

---
+++ src/main/java/com/max/springmaxdemobot/SpringMaxDemoBotApplication.java
@@ -0,0 +1,13 @@
+package com.max.springmaxdemobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMaxDemoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMaxDemoBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/max/springmaxdemobot/SpringMaxDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.springmaxdemobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMaxDemoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
